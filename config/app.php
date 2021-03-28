a6a650706a54ca84970799bd5b97fefbd6f9424d
NuraPress <43704843+chimilevschi@users.noreply.github.com>
2021-03-28 02:49:40 +0200

    Update

--- config/app.php
+++ config/app.php
@@ -186,8 +186,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         
-        Nestable\NestableServiceProvider::class,
-
     ],
 
     /*
@@ -224,7 +222,6 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Nestable' => Nestable\Facades\NestableService::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
